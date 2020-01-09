638f7cb1fc238e9a2ea553f7ca5e63cf1909b611
chucklapress <charleslapress@gmail.com>
2020-01-09 12:11:24 -0500

    working with classes

--- newest.groovy
+++ newest.groovy
@@ -2,8 +2,22 @@ def range = 0 ..10
 assert (0 ..10) instanceof List
 println range.collect()
 
-// eample of Coercion operator 
+// eample of Coercion operator
 Integer x = 12345
 String s = x as String
 
 println s
+
+def name = "Chuck"
+interface Greeter {
+    void greet(String name)
+}
+class SystemGreeter implements Greeter {
+    void greet(String name) {
+        println "Hello $name"
+    }
+}
+
+def greeter = new SystemGreeter()
+assert greeter instanceof Greeter
+println greeter
